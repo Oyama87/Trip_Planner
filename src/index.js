2628cae7fb296120a07cec62b8dc5db386344b48
Justin Taylor Goldberg <JustinTaylorG@Gmail.com>
2018-11-13 16:27:35 -0500

    secret key abstracted to ignored file and required

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const mapboxgl = require('mapbox-gl');
 
-mapboxgl.accessToken = "Blah";
-
+mapboxgl.accessToken = require("../mapboxToken").token;
+console.log(typeof mapboxgl.accessToken)
 const map = new mapboxgl.Map({
   container: "map",
   center: [-74.009, 40.705],
