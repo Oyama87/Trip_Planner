75b41087f5d3497b28db0190bc0946e64591f402
Melanie Hublard Hershman <melhhersh@gmail.com>
2018-11-13 16:46:30 -0500

    added the marker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const mapboxgl = require('mapbox-gl');
+const mapboxgl = require("mapbox-gl");
 
-mapboxgl.accessToken = "Blah";
+mapboxgl.accessToken = 'pk.eyJ1IjoibWVsaGhlcnNoIiwiYSI6ImNqb2c4a2lnNDBjanozcHBuZ2kxOW03dWgifQ.yPWvIuGgF3V-7NvD7axhng';
 
 const map = new mapboxgl.Map({
   container: "map",
@@ -8,3 +8,10 @@ const map = new mapboxgl.Map({
   zoom: 12,
   style: "mapbox://styles/mapbox/streets-v10"
 });
+
+let markerDomEl = document.createElement('div');
+markerDomEl.style.width = "32px";
+markerDomEl.style.heights = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker().setLngLat([-74.009151, 40.705086]).addTo(map);
