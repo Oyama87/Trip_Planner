b07046b1f66f48a1ab9c2c240c1bfb5f2ceda38c
Justin Taylor Goldberg <JustinTaylorG@Gmail.com>
2018-11-14 00:42:12 -0500

    more styles for UI elements, nav is now small sidebar. Map screen feels larger and more open.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const mapboxgl = require("mapbox-gl");
 const markerBuilder = require('./marker.js');
 
-mapboxgl.accessToken = "pk.eyJ1IjoibWVsaGhlcnNoIiwiYSI6ImNqb2c4a2lnNDBjanozcHBuZ2kxOW03dWgifQ.yPWvIuGgF3V-7NvD7axhng";
+mapboxgl.accessToken = require('../mapboxToken.js').token;
 const map = new mapboxgl.Map({
   container: "map",
   center: [-74.009, 40.705],
